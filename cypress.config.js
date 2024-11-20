6630c10ff0ca3ea40252a7c73ebf5ff416722bb1
AghaAmil <moravveji.amir.92@gmail.com>
2024-11-20 18:13:32 +0330

    cypress configuration and directory change

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,14 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
     e2e: {
         baseUrl: 'https://bookcart.azurewebsites.net/',
-        viewportWidth: 1280,
-        viewportHeight: 720,
         defaultCommandTimeout: 10000,
-        supportFile: false
+        supportFile: false,
+        excludeSpecPattern: ['**/1-getting-started', '**/2-advanced-examples'],
+        specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
     },
 });
